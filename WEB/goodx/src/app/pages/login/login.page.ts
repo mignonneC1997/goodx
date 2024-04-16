167b600a157455eff337a5e9ab35beb0d80e4e06
mignonnecilliers <mignonne@softwareworx.co.za>
2024-04-16 15:18:17 +0200

    #03 added login UI

---
+++ WEB/goodx/src/app/pages/login/login.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  public loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      // Implement your login logic here
+      // For demonstration purposes, redirect to a home page after successful login
+      this.router.navigate(['/home']);
+    }
+  }
+
+}
